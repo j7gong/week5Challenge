a15bcaa5452105876a89b3b298b2d68191cdcede
Jingsi <gongjingsi@hotmail.com>
2022-10-01 23:19:13 -0400

    adjust function to loop all time blocks to indicate if time block is in the past, present, or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,18 @@
 // Display current date
 $("#currentDay").text(moment().format('dddd, MMMM Do YYYY'));
 
-// Add color to indicate if time block is in the past, present, or future
-const time = $("#time-9").text(); 
-
-var colorCode = function () {
-    // var format = 'hh:mm:ss'; 
-    // var checkTime = moment('11:00:00', format);
+// Loop all time blocks to indicate if time block is in the past, present, or future
+$(".row").each(function(){
+    var eachTime = $(this).find("#time").text(); 
     var currentTime = moment();
-    var testTime = moment(time, 'h A');
-    var afterTime = moment(testTime).add(1, 'h');
-
-    // console.log("current Time: " + currentTime);
-    if (moment(currentTime).isSameOrAfter(testTime) && moment(currentTime).isSameOrBefore(afterTime)) {
-        $("#div-9").addClass("red");
-    } else if (moment(currentTime).isBefore(testTime)) {
-        $("#div-9").addClass("green");
-    } else if (moment(currentTime).isAfter(afterTime)) {
-        $("#div-9").addClass("gray");
+    // var currentTime = moment('11:00:00', 'hh:mm:ss');
+    var startTime = moment(eachTime, 'h A');
+    var endTime = moment(startTime).add(1, 'h');
+    if (moment(currentTime).isSameOrAfter(startTime) && moment(currentTime).isBefore(endTime)) {
+        $(this).find("#block").addClass("red");
+    } else if (moment(currentTime).isBefore(startTime)) {
+        $(this).find("#block").addClass("green");
+    } else if (moment(currentTime).isSameOrAfter(endTime)) {
+        $(this).find("#block").addClass("gray");
     };
-};
-
-colorCode();
+});
